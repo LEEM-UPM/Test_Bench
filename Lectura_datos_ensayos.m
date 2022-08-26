db3e5958a64b191a8c5f2582b46c7eed6d6b4b35
Carlos Serradilla Gil <110128600+CarlosSerradillaGil@users.noreply.github.com>
2022-08-26 20:39:03 +0200

    Delete Lectura_datos_ensayos.m

--- Lectura_datos_ensayos.m
+++
@@ -1,9 +0,0 @@
-file = fopen('DATOS');
-data = fread(file,'int')
-datap = (data-5700)*440.22/(15600-9365)*9.8/1000;
-tiempo = linspace(0,8960,448);
-plot(tiempo, datap)
-grid on
-xlabel('Time (ms)');
-ylabel('Thrust (N)');
-axis([0 9000 -10 80]);
